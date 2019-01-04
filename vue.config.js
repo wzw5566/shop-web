16631edeb174cd968d2f372b40af8cc56ad8fdfa
wzw5566 <wzw33874@qq.coom>
2019-01-04 16:40:24 +0800

    初步布局2

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // baseUrl: 'https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin',
+    // devServer:{
+    //
+    //     host: "localhost",
+    //     https: false, // https:{type:Boolean}
+
+
+
+
+    // }
+}
